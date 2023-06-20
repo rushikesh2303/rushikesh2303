6d2551f3829369c683a2a69553e936169f16c746
Rushikesh Kotwal <136912462+rushikesh2303@users.noreply.github.com>
2023-06-20 21:12:21 +0530

    CWH_39_Thread.java

--- CWH_39_Thread.java
+++ CWH_39_Thread.java
@@ -1,3 +1,33 @@
+public class CWH_39_Thread extends Thread {
+    String str;
+    int n;
+
+    CWH_39_Thread(String str, int n) {
+        this.str = str;
+        this.n = n;
+    }
+
+    public void run() {
+        try {
+            for (int i = 1; i <= n; i++) {
+                System.out.println(str+" "+i+"Times");
+            }
+            System.out.println("");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        CWH_39_Thread t1 = new CWH_39_Thread("COVID19", 10);
+        t1.start();
+        CWH_39_Thread t2 = new CWH_39_Thread("LOCKDOWN2020", 20);
+        t2.start();
+        CWH_39_Thread t3= new CWH_39_Thread("VACCINATED", 30);
+        t3.start();
+
+    }
+}
+
 //import java.io.*;
 //import java.lang.String;
 //public class CWH_39_Thread extends Thread {
@@ -31,32 +61,3 @@
 //
 //        }
 //    }
-public class CWH_39_Thread extends Thread {
-    String str;
-    int n;
-
-    CWH_39_Thread(String str, int n) {
-        this.str = str;
-        this.n = n;
-    }
-
-    public void run() {
-        try {
-            for (int i = 1; i <= n; i++) {
-                System.out.println(str+" "+i+"Times");
-            }
-            System.out.println("");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    public static void main(String[] args) {
-        CWH_39_Thread t1 = new CWH_39_Thread("COVID19", 10);
-        t1.start();
-        CWH_39_Thread t2 = new CWH_39_Thread("LOCKDOWN2020", 20);
-        t2.start();
-        CWH_39_Thread t3= new CWH_39_Thread("VACCINATED", 30);
-        t3.start();
-
-    }
-}
