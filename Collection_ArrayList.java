3975ef4f70dadce998bfdeef664e345270b92c96
Rushikesh Kotwal <136912462+rushikesh2303@users.noreply.github.com>
2023-06-22 18:45:49 +0530

    Collection_ArrayList.java

---
+++ Collection_ArrayList.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class DemoArrayList
+{
+  public static void main(String args[])
+  {
+    ArrayList b=new ArrayList();
+    b.add("Pune");
+    b.add("Patana");
+    b.add("Parbhani");
+    b.add("Satara");
+    System.out.println(b);
+  }
+}
